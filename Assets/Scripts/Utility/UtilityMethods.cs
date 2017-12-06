dd470602c783b3ac1f51c1bac47664c62e5b314e
Felipe Simoes <fs.simoes@gmail.com>
2017-12-05 22:57:32 -0500

    Waves

--- Assets/Scripts/Utility/UtilityMethods.cs
+++ Assets/Scripts/Utility/UtilityMethods.cs
@@ -35,5 +35,13 @@ public static class UtilityMethods {
     Vector2 screenPoint = Camera.main.WorldToScreenPoint(worldPosition);
     rectTransform.position = screenPoint;
   }
+    public static Quaternion SmoothlyLook(Transform fromTransform,    Vector3 toVector3) {
+        if (fromTransform.position == toVector3) {
+            return fromTransform.localRotation;
+        }
+        Quaternion currentRotation = fromTransform.localRotation;
+        Quaternion targetRotation = Quaternion.LookRotation(toVector3 -      fromTransform.position);
+        return Quaternion.Slerp(currentRotation, targetRotation,      Time.deltaTime * 10f);
+    }
 }
 
